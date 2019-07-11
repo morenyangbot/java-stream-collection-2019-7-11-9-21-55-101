76d4c714306a7d64c9f8a98cbefe21e37dec2eb7
Teng YANG <teng.yang@oocl.com>
2019-07-11 20:13:48 +0800

    Implement getListByInterval

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,11 +2,19 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        int index = left;
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0; i <= Math.abs(left - right); i++) {
+            integers.add(index);
+            if (left > right) index--;
+            else index++;
+        }
+        return integers;
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
